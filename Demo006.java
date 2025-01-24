ceeb449d8af9b61840d71e3f5b631dfce71a182e
RAJA-501 <2311cs010501@mallareddyuniversity.ac.in>
2025-01-24 11:17:56 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,13 @@
+
+
+public class Demo006 {
+    public static void main(String[] args) {
+        String Username = "raja";
+        String Password = "raja";
+        while ((Username == "raja") && (Password == "raja")) {
+            System.out.println("Hello AJAY Happy New Year");
+            break;
+        }
+    }
+
+}
---
+++ Demo007.java
@@ -0,0 +1,12 @@
+
+
+public class Demo007 {
+    public static void main(String[] args) {
+        String Name = "raja";
+        int Age = 20;
+        long PhNO = 1234567890;
+        while ((Name == "raja") && (Age == 20) && (PhNO == 1234567890)) {
+            break;
+        }
+    }
+}
